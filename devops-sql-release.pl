1ecca153688877cba9f0f4372d617ec85a01eb43
Eric Hermann <ericdavidhermann@gmail.com>
2018-09-23 20:37:47 -0400

    Main functionality complete.

--- devops-sql-release.pl
+++ devops-sql-release.pl
@@ -1,11 +1,11 @@
 #!C:\Strawberry\perl\bin\perl.exe
-#---------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # DEVOPS script for assisting with SQL script additions for release file
 # candidates.
 # Author: Eric Hermann, 2018
 # Email: ericdavidhermann@gmail.com
 # Licensed under GNU GPL v3.0
-#---------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 use strict;
 use warnings;
 
@@ -13,17 +13,26 @@ use Data::Dumper;
 use Getopt::Long;
 use v5.10.1;
 no warnings 'experimental';
+use Try::Tiny;
 
+# The version number for this script
 use constant VERSION => "1.0.0";
-# Ticketing system prefix (Like for Jira/Mantis/etc)
-use constant TKT_PREFIX  => 'TKT-';
-# Release file prefix
-use constant RF_PREFIX   => 'release-sql-';
+
+#----------
 # Can set your defaults according to your needs here, and override with
 # any GetOptions passed in.
+# Ticketing system prefix (Like for Jira/Mantis/etc)
+use constant TKT_PREFIX   => 'TKT-';
+# Release file prefix
+use constant RF_PREFIX    => 'release-';
+# The directory for your individual ticket-based SQL files
 use constant SQL_DIR      => 'data/';
+# The directory for your combined SQL release file
 use constant RELEASE_DIR  => 'data/releases/';
+# The release version for your application.
+# Not to be confused with the VERSION of this perl script!
 use constant RELEASE_VER  => 'v1.0';
+#----------
 
 # Initialize potential GetOptions
 my ($helpMe, $sqlDir, $relDir, $ticket, $version) = (undef)x5;
@@ -41,30 +50,58 @@ if (defined $helpMe) {
 }
 
 MAIN:
+    my $operation={successful=>1}; # Ever the optimist.
+
     printHeader();
 
-    # Ticket is mandatory.
+    # Ticket is mandatory and we will reprompt til we get one.
     while (!defined $ticket or $ticket eq '' or $ticket !~ /^\d*$/){
         $ticket = getTicket();
     }
-    # We have our ticket now.
-    print "\nThe ticket is: $ticket \n";
 
-    # Establish the working environment: use the constants defined above unless the
-    # user has specified new environment variables.
+    # Establish the working environment: use the constants defined above unless
+    # the user has specified new environment variables.
     if (!defined $sqlDir) { $sqlDir  = SQL_DIR; }
     if (!defined $relDir) { $relDir  = RELEASE_DIR; }
     if (!defined $version){ $version = RELEASE_VER; }
 
     # Let's begin
-    print Dumper($sqlDir, $relDir, $version);
+    # Build our SQL file and Release file path and name
+    my $sqlFile = $sqlDir.RELEASE_VER."/".TKT_PREFIX.$ticket.".sql";
+    # EX: data/TKT-1000.sql
+    my $relFile = $relDir . RF_PREFIX . RELEASE_VER. "/" .
+                  RF_PREFIX .$version."-final.sql";
+    # EX: data/releases/release-v1.0/release-v1.0-final.sql
 
-    print "\n\n\tAppending SQL in SQL file: ";
-    print "\n\t  ".$sqlDir.TKT_PREFIX.$ticket.".sql\n\n";
-    print "\tTo SQL Release file: ";
-    print "\n\t  ".$relDir.$version."/".RF_PREFIX.$version.".sql\n\n";
+    print "\n\n    Appending SQL in SQL file:\n";
+    print "      $sqlFile";
+    print "\n\n    To SQL Release file:\n";
+    print "      $relFile\n\n";
 
-    print "\tIs this correct [y/n]?";
+    my $confirm=undef;
+    print "    Is this correct? [y/n]: ";
+    $confirm=<>;
+
+    if ($confirm !~ /^y|Y|yes|YES/) {
+        printError('CONFIRM');
+        $operation->{successful} = 0;
+    }
+
+    # TODO: CHECK IF BOTH FILES EXIST BEFORE PROCEEDING
+
+    if ($operation->{successful}){
+        print "\n    Appending...\n";
+        $operation = appendToReleaseFile($sqlFile, $relFile);
+        if ($operation->{successful}) {
+            print "\n    ...Done\n";
+        } else {
+            print "\n    Abending Script\n";
+        }
+    }
+
+    $operation->{successful} ? exit 0 : exit 1;
+
+#/MAIN
 
 #-------------------------------------------------------------------------------
 # SUBROUTINES
@@ -79,12 +116,15 @@ sub printHeader {
     #   Nothing
     #*
     #---------------------------------------------------------------------------
-    print "\n";
-    print '═'x40;
-    print "\nThis script will concatenate the\n";
-    print "ticket's SQL file to the release SQL file\n\n";
-    print "\n";
-    print $0."\nVersion: " . VERSION. "\n\n";
+    my $div = '═'x55;
+    print "\n\n";
+    print "╔$div╗\n";
+    print "║  ".$0."\t\t\t\t║\n";
+    print "║  Version: " . VERSION ."\t\t\t\t\t║\n";
+    print "╠$div╣\n";
+    print "║  This script will concatenate the ticket's\t\t║\n";
+    print "║  SQL file to the release SQL file\t\t\t║\n";
+    print "╚$div╝\n";
 }
 
 sub helpMe {
@@ -137,12 +177,33 @@ sub printError {
     my $eClass = uc(shift);
     given ($eClass) {
         when ('TICKET'){
-            print "\n\n";
+            print "\n\n    ";
             print "[ERROR]: The ticket you entered was not numeric.\n";
             print "         Enter only the numeric part of the ticket!\n\n";
         }
+
+        when ('CONFIRM'){
+            print "\n\n    ";
+            print "[ERROR]: Aborting script execution at request of user.\n";
+            print "         Please rerun script after making necessary\n";
+            print "         changes.\n\n";
+        }
+
+        when ('UNDEF_FILE'){
+            print "\n\n    ";
+            print "[ERROR]: One or both of the file path/names is missing\n";
+            print "         Aborting appending of SQL to release file.\n\n";
+        }
+
+        when ('APPEND'){
+            print "\n\n    ";
+            print "[ERROR]: A problem was encountered while attempting\n";
+            print "         to append the SQL to the release file.\n";
+            print "         Aborting appending of SQL to release file.\n\n";
+        }
+
         default {
-            print "\n\n";
+            print "\n\n    ";
             print "[ERROR]: An unspecified fatal error occurred.\n";
             print "         Please consult --help for assistance.\n\n";
         }
@@ -151,7 +212,7 @@ sub printError {
 
 sub getTicket {
     #---------------------------------------------------------------------------
-    #** @method public printError
+    #** @method public getTicket
     #   Continually reprompt the user for a valid ticket number
     # Parameters:
     #   NONE
@@ -161,7 +222,7 @@ sub getTicket {
     #---------------------------------------------------------------------------
     my $ticket = undef;
     if (!defined $ticket) {
-        print "Please enter the ticket number: ";
+        print "    Please enter the ticket number: ";
         $ticket = <>;
     }
 
@@ -173,6 +234,45 @@ sub getTicket {
     }
 
     return $ticket;
+}
+
+sub appendToReleaseFile {
+    #---------------------------------------------------------------------------
+    #** @method public appendToReleaseFile
+    #   Appends the ticket sqlFile to the release candidate file relFile
+    # Parameters:
+    #   sqlFile : STRING - The path/name of the SQL file to be appended
+    #   relFile : STRING - The path/name of the release candidate SQL file
+    # Returns:
+    #   ticket  INT : The ticket number the script will use from here on out.
+    #*
+    #---------------------------------------------------------------------------
+    my ($sqlFile, $relFile) = @_;
+    my $result = {successful=>1}; # Assume this will succeed
+
+    # Make sure we have what we need with regard to file names.
+    if (!defined $sqlFile or $sqlFile eq '' or
+        !defined $relFile or $relFile eq '')
+    {
+        printError('UNDEF_FILE');
+        $result->{successful} = 0;
+    }
+
+    try {
+        open my $in_fh, '<', $sqlFile;
+        open my $out_fh, '>>', $relFile;
+        # Add a single new line before reading in file.
+        print $out_fh "\n";
+        local $/ = \65536; # Reading in 64kb chunks
+        while ( my $chunk = <$in_fh> ) { print $out_fh $chunk; }
+        # Add a single new line after reading in the file is complete.
+        print $out_fh "\n";
+    } catch {
+        printError('APPEND');
+        $result->{successful} = 0;
+    };
+
+    return $result;
 }
 
 1;
